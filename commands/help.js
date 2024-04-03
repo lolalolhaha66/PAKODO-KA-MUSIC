1a7dd408607e93ae3427ebd112ac8a56ce1123ee
lolalolhaha66 <164397709+lolalolhaha66@users.noreply.github.com>
2024-04-04 00:59:44 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -57,20 +57,20 @@ module.exports = {
            { name: '🎵 Now Playing', value: 'Display the currently playing song information' },
           { name: '🔊 Volume', value: 'Adjust the music volume [ hearing at high volumes is risky ]' },
         ) 
-       .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
+       .setImage('https://www.youtube.com/watch?v=p7YXXieghto&t=1s&ab_channel=Torwik')
       const button1 = new ButtonBuilder()
         .setLabel('YouTube')
-        .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+        .setURL('https://www.youtube.com/user/technothepig')
         .setStyle(ButtonStyle.Link);
 
       const button2 = new ButtonBuilder()
         .setLabel('Discord')
-        .setURL('https://discord.gg/FUEHs7RCqz')
+        .setURL('https://www.youtube.com/user/technothepig')
         .setStyle(ButtonStyle.Link);
 
       const button3 = new ButtonBuilder()
         .setLabel('Code')
-        .setURL('https://github.com/RTX-GAMINGG/RTX-MUSIC-BOT-v3.6')
+        .setURL('https://www.youtube.com/user/technothepig')
         .setStyle(ButtonStyle.Link);
 
       const row = new ActionRowBuilder()
