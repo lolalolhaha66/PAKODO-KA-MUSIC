1d3857547934c690a2a077ef2ffb094bf187e99c
lolalolhaha66 <164397709+lolalolhaha66@users.noreply.github.com>
2024-04-04 01:10:28 +0530

    Update playlist.js

--- commands/playlist.js
+++ commands/playlist.js
@@ -258,7 +258,7 @@ await interaction.editReply({
           url: 'https://discord.gg/FUEHs7RCqz'
         })
         .setDescription(`Hey <@${interaction.member.id}>, your song has been Added successfully! ✨`)
-        .setFooter({ text: 'YouTube - RTX GAMING' })
+        .setFooter({ text: 'YouTube - Technoblade Never Dies!' })
         await interaction.reply({
   content: '',
   embeds: [ loadingembed ] 
